623c1ae8cb054ada672982333b0e242ca35326b0
Guilherme Amoroso Romão <gromao@primeit.pt>
2023-01-04 23:15:23 +0000

    Added integration test

---
+++ src/test/java/co/gromao/coffeeshop/IntegrationTest.java
@@ -0,0 +1,31 @@
+package co.gromao.coffeeshop;
+
+import co.gromao.coffeeshop.data.dto.ResultDto;
+import co.gromao.coffeeshop.data.entity.Coordinates;
+import co.gromao.coffeeshop.data.repository.LocationsRepository;
+import co.gromao.coffeeshop.service.FindService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class IntegrationTest {
+
+    LocationsRepository repository = new LocationsRepository();
+    FindService findService = new FindService(repository);
+
+    @Test
+    void testFindNearestCoffeeShops() {
+        final List<ResultDto> test = findService.getNearestShops(new Coordinates(0, 0));
+
+        Assertions.assertEquals(repository.findCoffeeShops().size(), test.size());
+
+        for (int i = 0; i < test.size() - 1; i++) {
+            double distance1 = test.get(i).getDistanceToUserInMetres();
+            double distance2 = test.get(i + 1).getDistanceToUserInMetres();
+
+            Assertions.assertTrue(distance1 <= distance2);
+        }
+    }
+
+}
